e2d1641fa9c1956f5ae62b17dc9e912cb1f37e6a
Vidhi Kashyap <vidhikashyap9307@gmail.com>
2026-07-17 01:44:23 +0530

    Allow Vercel frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173"
+        "https://task-flow-yt1h.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
